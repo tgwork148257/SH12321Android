e6174c7c7503742dd2070132149964f8cda01ca7
Hebe <1252680267@qq.com>
2016-07-24 11:51:47 +0800

    update

--- app/src/main/java/com/hebe/report/activity/PhoneVerifyResultActivity.java
+++ app/src/main/java/com/hebe/report/activity/PhoneVerifyResultActivity.java
@@ -2,12 +2,21 @@ package com.hebe.report.activity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.hebe.report.R;
 import com.hebe.report.base.BaseActivity;
+import com.hebe.report.bean.CommonResultBean;
+import com.hebe.report.bean.SwindResultBean;
+import com.hebe.report.bean.VerifyCodeBean;
+import com.hebe.report.bean.VerifyResultBean;
+import com.hebe.report.utils.Utils;
 
+import org.xutils.common.Callback;
+import org.xutils.http.RequestParams;
+import org.xutils.view.annotation.Event;
 import org.xutils.view.annotation.ViewInject;
 import org.xutils.x;
 
@@ -18,10 +27,39 @@ import org.xutils.x;
 
 public class PhoneVerifyResultActivity extends BaseActivity {
 
+    @ViewInject(R.id.textview1)
+    TextView textview1;
+    @ViewInject(R.id.textview2)
+    TextView textview2;
+    @ViewInject(R.id.textview3)
+    TextView textview3;
+    @ViewInject(R.id.textview4)
+    TextView textview4;
+    @ViewInject(R.id.textview5)
+    TextView textview5;
+    @ViewInject(R.id.textview6)
+    TextView textview6;
+    @ViewInject(R.id.textview7)
+    TextView textview7;
+    @ViewInject(R.id.textview8)
+    TextView textview8;
+    @ViewInject(R.id.textview9)
+    TextView textview9;
+    @ViewInject(R.id.check1)
+    ImageView check1;
+    @ViewInject(R.id.check2)
+    ImageView check2;
+    @ViewInject(R.id.summit)
+    Button summit;
+
     @ViewInject(R.id.navi_title)
     private TextView navi_title;
     @ViewInject(R.id.navi_back)
     private ImageView navi_back;
+
+    private String jwid;
+    private int check = -1;
+    private int isget = -1;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,5 +74,107 @@ public class PhoneVerifyResultActivity extends BaseActivity {
             }
         });
         navi_title.setText("处理结果");
+        jwid = getIntent().getStringExtra("jwid");
+        getinfo();
+        showProgressDialog("正在加载");
+    }
+
+    public void getinfo() {
+        RequestParams params = Utils.getDefaultParams("App/getReportDetails");
+        params.addBodyParameter("user_token", Utils.getUserToken(this));
+        params.addBodyParameter("jw_id", jwid);
+        x.http().post(params, new Callback.CommonCallback<String>() {
+            @Override
+            public void onSuccess(String result) {
+                closeProgressDialog();
+                VerifyResultBean bean = Utils.jsonParase(result, VerifyResultBean.class);
+                if (bean != null && bean.getCode() == 200) {
+                    textview1.setText(bean.getData().getType_name());
+                    textview2.setText(bean.getData().getReport_type());
+                    textview3.setText(bean.getData().getReason());
+//                    textview4.setText(bean.getData().());
+                    textview5.setText(bean.getData().getBuy_mobile());
+                    textview6.setText(bean.getData().getBuy_time());
+                    textview7.setText(bean.getData().getOperator());
+                    textview8.setText("实体店".equals(bean.getData().getReport_type())?bean.getData().getAddress():bean.getData().getStore_sale_website());
+//                    textview9.setText(bean.getData().getc());
+                    isget = 1;
+                } else {
+                    showToast("请重试");
+                }
+            }
+
+            @Override
+            public void onError(Throwable ex, boolean isOnCallback) {
+                closeProgressDialog();
+            }
+
+            @Override
+            public void onCancelled(CancelledException cex) {
+            }
+
+            @Override
+            public void onFinished() {
+
+            }
+        });
+    }
+
+    public void submit(){
+        RequestParams params = Utils.getDefaultParams("App/reportFeedback");
+        params.addBodyParameter("user_token", Utils.getUserToken(this));
+        params.addBodyParameter("jw_id", jwid);
+        params.addBodyParameter("feedback", check+"");
+        x.http().post(params, new Callback.CommonCallback<String>() {
+            @Override
+            public void onSuccess(String result) {
+                closeProgressDialog();
+                CommonResultBean bean = Utils.jsonParase(result, CommonResultBean.class);
+                if (bean != null && bean.getCode() == 200) {
+                    showToast("反馈成功");
+                    finish();
+                } else {
+                    showToast("请重试");
+                }
+            }
+
+            @Override
+            public void onError(Throwable ex, boolean isOnCallback) {
+                closeProgressDialog();
+            }
+
+            @Override
+            public void onCancelled(CancelledException cex) {
+            }
+
+            @Override
+            public void onFinished() {
+
+            }
+        });
+    }
+
+    @Event({R.id.check1, R.id.check2, R.id.summit})
+    private void click(View view) {
+        switch (view.getId()) {
+            case R.id.check1:
+                check = 1;
+                check1.setImageResource(R.drawable.but_checked);
+                check2.setImageResource(R.drawable.but_uncheck);
+                break;
+            case R.id.check2:
+                check = 2;
+                check2.setImageResource(R.drawable.but_checked);
+                check1.setImageResource(R.drawable.but_uncheck);
+                break;
+            case R.id.summit:
+                if (isget==1&&check!=-1){
+                    showProgressDialog("正在提交");
+                    submit();
+                }else {
+                    showToast("请填写相关信息");
+                }
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/hebe/report/bean/VerifyResultBean.java
@@ -0,0 +1,174 @@
+package com.hebe.report.bean;
+
+/**
+ * Created by Hebe on 7-24-024.
+ */
+
+public class VerifyResultBean {
+
+    private int code;
+    private DataBean data;
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public DataBean getData() {
+        return data;
+    }
+
+    public void setData(DataBean data) {
+        this.data = data;
+    }
+
+    public static class DataBean {
+        private String type_name;
+        private Object store_website;
+        private String store_sale_website;
+        private String buy_mobile;
+        private String report_type;
+        private String operator;
+        private String buy_time;
+        private String address;
+        private String img;
+        private String card_img;
+        private String reason;
+        private String type;
+        private String add_time;
+        private String mobile;
+        private String con_flag;
+        private String jw_id;
+
+        public String getType_name() {
+            return type_name;
+        }
+
+        public void setType_name(String type_name) {
+            this.type_name = type_name;
+        }
+
+        public Object getStore_website() {
+            return store_website;
+        }
+
+        public void setStore_website(Object store_website) {
+            this.store_website = store_website;
+        }
+
+        public String getStore_sale_website() {
+            return store_sale_website;
+        }
+
+        public void setStore_sale_website(String store_sale_website) {
+            this.store_sale_website = store_sale_website;
+        }
+
+        public String getBuy_mobile() {
+            return buy_mobile;
+        }
+
+        public void setBuy_mobile(String buy_mobile) {
+            this.buy_mobile = buy_mobile;
+        }
+
+        public String getReport_type() {
+            return report_type;
+        }
+
+        public void setReport_type(String report_type) {
+            this.report_type = report_type;
+        }
+
+        public String getOperator() {
+            return operator;
+        }
+
+        public void setOperator(String operator) {
+            this.operator = operator;
+        }
+
+        public String getBuy_time() {
+            return buy_time;
+        }
+
+        public void setBuy_time(String buy_time) {
+            this.buy_time = buy_time;
+        }
+
+        public String getAddress() {
+            return address;
+        }
+
+        public void setAddress(String address) {
+            this.address = address;
+        }
+
+        public String getImg() {
+            return img;
+        }
+
+        public void setImg(String img) {
+            this.img = img;
+        }
+
+        public String getCard_img() {
+            return card_img;
+        }
+
+        public void setCard_img(String card_img) {
+            this.card_img = card_img;
+        }
+
+        public String getReason() {
+            return reason;
+        }
+
+        public void setReason(String reason) {
+            this.reason = reason;
+        }
+
+        public String getType() {
+            return type;
+        }
+
+        public void setType(String type) {
+            this.type = type;
+        }
+
+        public String getAdd_time() {
+            return add_time;
+        }
+
+        public void setAdd_time(String add_time) {
+            this.add_time = add_time;
+        }
+
+        public String getMobile() {
+            return mobile;
+        }
+
+        public void setMobile(String mobile) {
+            this.mobile = mobile;
+        }
+
+        public String getCon_flag() {
+            return con_flag;
+        }
+
+        public void setCon_flag(String con_flag) {
+            this.con_flag = con_flag;
+        }
+
+        public String getJw_id() {
+            return jw_id;
+        }
+
+        public void setJw_id(String jw_id) {
+            this.jw_id = jw_id;
+        }
+    }
+}
